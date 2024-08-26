8daca056b7a20fef63c195526555f9e979932a99
Ninad-arakh <arakhninad@gmail.com>
2024-08-27 00:08:33 +0530

    first commit again

---
+++ src/Components/Watch.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import TitlePage from "./MovieDetails/TitlePage";
+import useMovieDetails from "../Hooks/useMovieDetails";
+
+const Watch = () => {
+  const [searchParam] = useSearchParams();
+  const id = searchParam.get("jbv");
+  const dispatch = useDispatch();
+  const key = useSelector((store) => store.movie.ClickedTrailer);
+
+  useMovieDetails(id);
+
+  if (!key) return null;
+  //   console.log(key);
+  return (
+    <div className=" bg-neutral-900 m-0 p-0  w-screen xs:w-screen">
+      <div className="flex justify-around w-screen">
+        <iframe
+            className="w-[70%]  xs:w-screen "
+          height={520}
+          width={980}
+          src={
+            "https://www.youtube.com/embed/" +
+            key.key +
+            "?autoplay=1&mute=1&loop=1"
+          }
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div className="flex justify-center">
+        <TitlePage />
+      </div>
+    </div>
+  );
+};
+
+export default Watch;
